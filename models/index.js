eae4afad97cd2128dc9bd405028c1ebc89c1ec50
curriecoder <andrew.j.currie0@gmail.com>
2021-10-15 16:02:09 -0400

    started product routes, saving progress

--- models/index.js
+++ models/index.js
@@ -9,10 +9,12 @@ Product.belongsTo(Category, {
   foreignKey: 'category_id',
   onDelete: "SET NULL"
 });
+
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
+
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: {
